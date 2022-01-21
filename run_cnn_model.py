105ea267b88f3b74853a9fa38a27833fbe2ef629
赵宇 <zhaoyu@vpn37-171.remote-access.kth.se>
2022-01-21 18:44:25 +0100

    update models

--- run_cnn_model.py
+++ run_cnn_model.py
@@ -13,10 +13,12 @@ from keras_unet_collection import models
 
 def get_dateset(batch_size):
 
-    train_dataset = np.load('/geoinfo_vol1/zhao2/proj3_train_img_v2.npy').reshape((-1,6,224,224)).transpose((0,2,3,1))
+    train_dataset = np.load('/geoinfo_vol1/zhao2/proj2_dataset/proj2_train.npy')
+    val_dataset = np.load('/geoinfo_vol1/zhao2/proj2_dataset/proj2_test.npy')
     print(train_dataset.shape)
-    y_dataset = train_dataset[:,:,:,5]>0
-    x_train, x_val, y_train, y_val = train_test_split(train_dataset[:,:,:,:5].astype(float), y_dataset.astype(float), test_size=0.2)
+    y_dataset = train_dataset[:,:,:,3]>0
+    y_dataset_val = val_dataset[:,:,:,3]>0
+    x_train, x_val, y_train, y_val = train_dataset[:,:,:,:3], val_dataset[:,:,:,:3], y_dataset, y_dataset_val
     def make_generator(inputs, labels):
         def _generator():
             for input, label in zip(inputs, labels):
@@ -47,7 +49,7 @@ def dice_coef(y_true, y_pred):
 
 def wandb_config(model_name, backbone):
     wandb.login()
-    wandb.init(project=str(model_name)+'_'+str(backbone), entity="zhaoyutim")
+    wandb.init(project='proj2_'+str(model_name)+'_'+str(backbone), entity="zhaoyutim")
     # wandb.config = {
     #   "learning_rate": learning_rate,
     #   "weight_decay": weight_decay,
@@ -81,14 +83,14 @@ if __name__=='__main__':
     strategy = tf.distribute.MirroredStrategy()
     with strategy.scope():
         if model_name == 'fpn':
-            input = tf.keras.Input(shape=(None, None, 5))
+            input = tf.keras.Input(shape=(None, None, 3))
             conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
             basemodel = FPN(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
             output = basemodel(conv1)
             model = tf.keras.Model(input, output, name=model_name)
 
         elif model_name == 'unet':
-            input = tf.keras.Input(shape=(None, None, 5))
+            input = tf.keras.Input(shape=(None, None, 3))
             conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
             if backbone == None:
                 basemodel = Unet(encoder_weights='imagenet', activation='sigmoid')
@@ -99,39 +101,39 @@ if __name__=='__main__':
             model = tf.keras.Model(input, output, name=model_name)
 
         elif model_name == 'linknet':
-            input = tf.keras.Input(shape=(None, None, 5))
+            input = tf.keras.Input(shape=(None, None, 3))
             conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
             basemodel = Linknet(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
             output = basemodel(conv1)
             model = tf.keras.Model(input, output, name=model_name)
 
         elif model_name == 'pspnet':
-            input = tf.keras.Input(shape=(None, None, 5))
+            input = tf.keras.Input(shape=(None, None, 3))
             input_resize = tf.keras.layers.Resizing(384,384)(input)
             conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input_resize)
             basemodel = PSPNet(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
             output = basemodel(conv1)
-            output_resize = tf.keras.layers.Resizing(224,224)(output)
+            output_resize = tf.keras.layers.Resizing(256,256)(output)
             model = tf.keras.Model(input, output_resize, name=model_name)
         elif model_name == 'swinunet':
-            input = tf.keras.Input(shape=(None, None, 5))
+            input = tf.keras.Input(shape=(None, None, 3))
             conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
-            basemodel = models.swin_unet_2d((224, 224, 3), filter_num_begin=64, n_labels=1, depth=4, stack_num_down=2, stack_num_up=2,
+            basemodel = models.swin_unet_2d((256, 256, 3), filter_num_begin=64, n_labels=1, depth=4, stack_num_down=2, stack_num_up=2,
                                         patch_size=(2, 2), num_heads=[4, 8, 8, 8], window_size=[4, 2, 2, 2], num_mlp=512,
                                         output_activation='Sigmoid', shift_window=True, name='swin_unet')
             output = basemodel(conv1)
             model = tf.keras.Model(input, output, name=model_name)
         elif model_name == 'transunet':
-            input = tf.keras.Input(shape=(None, None, 5))
+            input = tf.keras.Input(shape=(None, None, 3))
             conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
-            basemodel = models.transunet_2d((224, 224, 3), filter_num=[64, 128, 256, 512], n_labels=1, stack_num_down=2, stack_num_up=2,
+            basemodel = models.transunet_2d((256, 256, 3), filter_num=[64, 128, 256, 512], n_labels=1, stack_num_down=2, stack_num_up=2,
                                         embed_dim=256, num_mlp=768, num_heads=3, num_transformer=12,
                                         activation='ReLU', mlp_activation='GELU', output_activation='Sigmoid',
                                         batch_norm=True, pool=True, unpool='bilinear', name='transunet')
             output = basemodel(conv1)
             model = tf.keras.Model(input, output, name=model_name)
         elif model_name == 'unet_2d':
-            input = tf.keras.Input(shape=(None, None, 5))
+            input = tf.keras.Input(shape=(None, None, 3))
             conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
             basemodel = models.unet_2d((None, None, 3), [64, 128, 256, 512, 1024], n_labels=1,
                                        stack_num_down=2, stack_num_up=1,
@@ -153,7 +155,7 @@ if __name__=='__main__':
     val_dataset = val_dataset.with_options(options)
 
     if load_weights== 'yes':
-        model.load_weights('/geoinfo_vol1/zhao2/proj3_'+model_name+'_pretrained_'+backbone)
+        model.load_weights('/geoinfo_vol1/zhao2/proj2_model/proj2_'+model_name+'_pretrained_'+backbone)
     else:
         print('training in progress')
         history = model.fit(
@@ -163,6 +165,6 @@ if __name__=='__main__':
             validation_data=val_dataset,
             validation_steps=validation_steps,
             epochs=MAX_EPOCHS,
-            callbacks=[WandbCallback()],
+            # callbacks=[WandbCallback()],
         )
-        model.save('/geoinfo_vol1/zhao2/proj3_'+model_name+'_pretrained_'+backbone)
+        model.save('/geoinfo_vol1/zhao2/proj2_model/proj2_'+model_name+'_pretrained_'+backbone)
