3839d260244d315faecd79478e0a54cb97a1f19d
赵宇 <zhaoyu@vpn10-139.remote-access.kth.se>
2022-06-28 21:53:42 +0200

    update cnn model

--- run_cnn_model.py
+++ run_cnn_model.py
@@ -54,10 +54,10 @@ def dice_coef(y_true, y_pred):
     smooth = 1.0
     return 1-(2.0*intersection+smooth)/(tf.math.reduce_sum(y_true_f)+tf.math.reduce_sum(y_pred_f)+smooth)
 
-def wandb_config(model_name, backbone, batch_size, learning_rate):
+def wandb_config(model_name, backbone, batch_size, learning_rate, data):
     wandb.login()
     wandb.init(project='proj2_palsar', entity="zhaoyutim")
-    wandb.run.name = 'model_name' + str(model_name) + 'backbone_'+ str(backbone)+ 'batchsize_'+str(batch_size)+'learning_rate_'+str(learning_rate)
+    wandb.run.name = 'model_name' + str(model_name) + 'backbone_'+ str(backbone)+ 'batchsize_'+str(batch_size)+'learning_rate_'+str(learning_rate)+'data_'+str(data)
     wandb.config = {
       "learning_rate": learning_rate,
       "weight_decay": weight_decay,
@@ -89,7 +89,7 @@ if __name__=='__main__':
 
     train_dataset, val_dataset, steps_per_epoch, validation_steps = get_dateset(batch_size, data)
 
-    wandb_config(model_name, backbone, batch_size, learning_rate)
+    wandb_config(model_name, backbone, batch_size, learning_rate, data)
 
     strategy = tf.distribute.MirroredStrategy()
 
@@ -212,4 +212,4 @@ if __name__=='__main__':
             epochs=MAX_EPOCHS,
             callbacks=[WandbCallback()],
         )
-        model.save('/geoinfo_vol1/zhao2/proj2_model/proj2_'+model_name+'_pretrained_'+backbone)
+        model.save('/geoinfo_vol1/zhao2/proj2_model/proj2_'+model_name+'_pretrained_'+backbone+'dataset_'+data)
