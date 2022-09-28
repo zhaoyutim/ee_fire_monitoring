1070ea718417b748141d5d068fa04b9c14b272ce
赵宇 <zhaoyu@vpn37-129.remote-access.kth.se>
2022-09-28 13:10:04 +0200

    gedi model

--- run_cnn_model_gedi.py
+++ run_cnn_model_gedi.py
@@ -99,7 +99,7 @@ if __name__=='__main__':
 
     wandb_config(model_name, backbone, batch_size, learning_rate)
 
-    strategy = tf.distribute.MirroredStrategy()
+    # strategy = tf.distribute.MirroredStrategy()
 
     data_augmentation = tf.keras.Sequential([
         tf.keras.layers.RandomFlip("horizontal_and_vertical"),
@@ -109,58 +109,58 @@ if __name__=='__main__':
         tf.keras.layers.Resizing(256, 256),
         tf.keras.layers.Rescaling(1./255)
     ])
-    with strategy.scope():
-        if model_name == 'fpn':
-            input = tf.keras.Input(shape=(None, None, 3))
-            # x = resize_and_rescale(input)
-            # x = data_augmentation(x)
-            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
-            basemodel = FPN(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
-            output = basemodel(conv1)
-            model = tf.keras.Model(input, output, name=model_name)
+    # with strategy.scope():
+    if model_name == 'fpn':
+        input = tf.keras.Input(shape=(None, None, 3))
+        # x = resize_and_rescale(input)
+        # x = data_augmentation(x)
+        conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+        basemodel = FPN(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
+        output = basemodel(conv1)
+        model = tf.keras.Model(input, output, name=model_name)
 
-        elif model_name == 'unet':
-            input = tf.keras.Input(shape=(None, None, 3))
-            # x = resize_and_rescale(input)
-            # x = data_augmentation(x)
-            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
-            if backbone == 'None':
-                basemodel = Unet(encoder_weights='imagenet', activation='sigmoid')
-            else:
-                basemodel = Unet(backbone, encoder_weights='imagenet', activation='sigmoid')
-            output = basemodel(conv1)
-            model = tf.keras.Model(input, output, name=model_name)
+    elif model_name == 'unet':
+        input = tf.keras.Input(shape=(None, None, 3))
+        # x = resize_and_rescale(input)
+        # x = data_augmentation(x)
+        conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+        if backbone == 'None':
+            basemodel = Unet(encoder_weights='imagenet', activation='sigmoid')
+        else:
+            basemodel = Unet(backbone, encoder_weights='imagenet', activation='sigmoid')
+        output = basemodel(conv1)
+        model = tf.keras.Model(input, output, name=model_name)
 
-        elif model_name == 'linknet':
-            input = tf.keras.Input(shape=(None, None, 3))
-            # x = resize_and_rescale(input)
-            # x = data_augmentation(x)
-            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
-            basemodel = Linknet(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
-            output = basemodel(conv1)
-            model = tf.keras.Model(input, output, name=model_name)
+    elif model_name == 'linknet':
+        input = tf.keras.Input(shape=(None, None, 3))
+        # x = resize_and_rescale(input)
+        # x = data_augmentation(x)
+        conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+        basemodel = Linknet(backbone, encoder_weights='imagenet', activation='sigmoid', classes=1)
+        output = basemodel(conv1)
+        model = tf.keras.Model(input, output, name=model_name)
 
-        elif model_name == 'pspnet':
-            input = tf.keras.Input(shape=(None, None, 3))
-            # x = resize_and_rescale(input)
-            # x = data_augmentation(x)
-            input_resize = tf.keras.layers.Resizing(384,384)(input)
-            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input_resize)
-            basemodel = PSPNet(backbone, activation='sigmoid', classes=1)
-            output = basemodel(conv1)
-            output_resize = tf.keras.layers.Resizing(256,256)(output)
-            model = tf.keras.Model(input, output_resize, name=model_name)
-        elif model_name == 'swinunet':
-            input = tf.keras.Input(shape=(256, 256, 3))
-            input_resize = tf.keras.layers.Resizing(224,224)(input)
-            # basemodel = models.swin_unet_2d((224, 224, 3), filter_num_begin=64, n_labels=1, depth=4, stack_num_down=2, stack_num_up=2,
-            #                             patch_size=(2, 2), num_heads=[3, 6, 12, 24], window_size=[7, 7, 7, 7], num_mlp=512,
-            #                             output_activation='Sigmoid', shift_window=True, name='swin_unet')
-            basemodel = SwinTransformer('swin_tiny_224', num_classes=1, include_top=False, pretrained=False)
+    elif model_name == 'pspnet':
+        input = tf.keras.Input(shape=(None, None, 3))
+        # x = resize_and_rescale(input)
+        # x = data_augmentation(x)
+        input_resize = tf.keras.layers.Resizing(384,384)(input)
+        conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input_resize)
+        basemodel = PSPNet(backbone, activation='sigmoid', classes=1)
+        output = basemodel(conv1)
+        output_resize = tf.keras.layers.Resizing(256,256)(output)
+        model = tf.keras.Model(input, output_resize, name=model_name)
+    elif model_name == 'swinunet':
+        input = tf.keras.Input(shape=(256, 256, 3))
+        input_resize = tf.keras.layers.Resizing(224,224)(input)
+        # basemodel = models.swin_unet_2d((224, 224, 3), filter_num_begin=64, n_labels=1, depth=4, stack_num_down=2, stack_num_up=2,
+        #                             patch_size=(2, 2), num_heads=[3, 6, 12, 24], window_size=[7, 7, 7, 7], num_mlp=512,
+        #                             output_activation='Sigmoid', shift_window=True, name='swin_unet')
+        basemodel = SwinTransformer('swin_tiny_224', num_classes=1, include_top=False, pretrained=False)
 
-            output = basemodel(input_resize)
-            output_resize = tf.keras.layers.Resizing(256,256)(output)
-            model = tf.keras.Model(input, output_resize, name=model_name)
+        output = basemodel(input_resize)
+        output_resize = tf.keras.layers.Resizing(256,256)(output)
+        model = tf.keras.Model(input, output_resize, name=model_name)
     model.summary()
     # checkpoint = ModelCheckpoint(
     #     '/geoinfo_vol1/zhao2/proj4_model/proj4_' + model_name + '_pretrained_' + backbone,
@@ -174,10 +174,10 @@ if __name__=='__main__':
     masked_mse = masked_mse(np.nan)
     model.compile(optimizer, loss=masked_mse, metrics= masked_mse)
 
-    options = tf.data.Options()
-    options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
-    train_dataset = train_dataset.with_options(options)
-    val_dataset = val_dataset.with_options(options)
+    # options = tf.data.Options()
+    # options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
+    # train_dataset = train_dataset.with_options(options)
+    # val_dataset = val_dataset.with_options(options)
 
     print('training in progress')
     history = model.fit(
