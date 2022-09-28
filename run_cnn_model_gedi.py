7a25fea02a4ad63d07a3d7f72c20a232204442ee
赵宇 <zhaoyu@vpn37-129.remote-access.kth.se>
2022-09-28 12:51:37 +0200

    gedi model

--- run_cnn_model_gedi.py
+++ run_cnn_model_gedi.py
@@ -30,13 +30,13 @@ def set_global_seed(seed=21):
     # Python
     random.seed(seed)
 
-def get_dateset_gedi(batch_size, nchannels):
+def get_dateset_gedi(batch_size):
     if platform.system()=='Darwin':
         train_array = np.load('dataset/proj4_train_na' + '.npy')
     else:
         train_array = np.load('/geoinfo_vol1/zhao2/proj4_dataset/proj4_train_na'+'.npy')
-    y_dataset = train_array[:,:,:,nchannels]>0
-    x_train, x_val, y_train, x_val = train_test_split(train_array[:,:,:,:nchannels], y_dataset, test_size=0.2, random_state=0)
+    y_dataset = train_array[:,:,:,8]>0
+    x_train, x_val, y_train, x_val = train_test_split(train_array[:,:,:,:3], y_dataset, test_size=0.2, random_state=0)
     def make_generator(inputs, labels):
         def _generator():
             for input, label in zip(inputs, labels):
@@ -65,10 +65,10 @@ def masked_mse(mask_value):
     f.__name__ = 'Masked_MSE_value{}'.format(mask_value)
     return f
 
-def wandb_config(model_name, backbone, batch_size, learning_rate, nchannels):
+def wandb_config(model_name, backbone, batch_size, learning_rate):
     wandb.login()
     wandb.init(project='proj4_gedi', entity="zhaoyutim")
-    wandb.run.name = 'model_name' + str(model_name) + 'backbone_'+ str(backbone)+ 'batchsize_'+str(batch_size)+'learning_rate_'+str(learning_rate)+'_nchannels_'+str(nchannels)
+    wandb.run.name = 'model_name' + str(model_name) + 'backbone_'+ str(backbone)+ 'batchsize_'+str(batch_size)+'learning_rate_'+str(learning_rate)
     wandb.config = {
       "learning_rate": learning_rate,
       "weight_decay": weight_decay,
@@ -93,12 +93,11 @@ if __name__=='__main__':
     MAX_EPOCHS=100
     fine_tune=False
     learning_rate = args.lr
-    nchannels = args.nc
     weight_decay = learning_rate/10
     set_global_seed()
-    train_dataset, val_dataset, steps_per_epoch, validation_steps = get_dateset_gedi(batch_size, nchannels)
+    train_dataset, val_dataset, steps_per_epoch, validation_steps = get_dateset_gedi(batch_size)
 
-    wandb_config(model_name, backbone, batch_size, learning_rate, nchannels)
+    wandb_config(model_name, backbone, batch_size, learning_rate)
 
     strategy = tf.distribute.MirroredStrategy()
 
@@ -112,7 +111,7 @@ if __name__=='__main__':
     ])
     with strategy.scope():
         if model_name == 'fpn':
-            input = tf.keras.Input(shape=(None, None, nchannels))
+            input = tf.keras.Input(shape=(None, None, 3))
             # x = resize_and_rescale(input)
             # x = data_augmentation(x)
             conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
@@ -121,7 +120,7 @@ if __name__=='__main__':
             model = tf.keras.Model(input, output, name=model_name)
 
         elif model_name == 'unet':
-            input = tf.keras.Input(shape=(None, None, nchannels))
+            input = tf.keras.Input(shape=(None, None, 3))
             # x = resize_and_rescale(input)
             # x = data_augmentation(x)
             conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
@@ -133,7 +132,7 @@ if __name__=='__main__':
             model = tf.keras.Model(input, output, name=model_name)
 
         elif model_name == 'linknet':
-            input = tf.keras.Input(shape=(None, None, nchannels))
+            input = tf.keras.Input(shape=(None, None, 3))
             # x = resize_and_rescale(input)
             # x = data_augmentation(x)
             conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
@@ -142,7 +141,7 @@ if __name__=='__main__':
             model = tf.keras.Model(input, output, name=model_name)
 
         elif model_name == 'pspnet':
-            input = tf.keras.Input(shape=(None, None, nchannels))
+            input = tf.keras.Input(shape=(None, None, 3))
             # x = resize_and_rescale(input)
             # x = data_augmentation(x)
             input_resize = tf.keras.layers.Resizing(384,384)(input)
@@ -152,7 +151,7 @@ if __name__=='__main__':
             output_resize = tf.keras.layers.Resizing(256,256)(output)
             model = tf.keras.Model(input, output_resize, name=model_name)
         elif model_name == 'swinunet':
-            input = tf.keras.Input(shape=(256, 256, nchannels))
+            input = tf.keras.Input(shape=(256, 256, 3))
             input_resize = tf.keras.layers.Resizing(224,224)(input)
             # basemodel = models.swin_unet_2d((224, 224, 3), filter_num_begin=64, n_labels=1, depth=4, stack_num_down=2, stack_num_up=2,
             #                             patch_size=(2, 2), num_heads=[3, 6, 12, 24], window_size=[7, 7, 7, 7], num_mlp=512,
@@ -190,4 +189,4 @@ if __name__=='__main__':
         epochs=MAX_EPOCHS,
         callbacks=[WandbCallback(), checkpoint],
     )
-    model.save('/geoinfo_vol1/zhao2/proj4_model/proj4_'+model_name+'_pretrained_'+backbone+'_dataset_'+'_nchannels_'+str(nchannels))
+    model.save('/geoinfo_vol1/zhao2/proj4_model/proj4_'+model_name+'_pretrained_'+backbone)
