0afdb0362fd95c188fdc6529e59507ff6dc29a37
赵宇 <zhaoyu@vpn10-147.remote-access.kth.se>
2022-10-28 12:53:29 +0200

    fix mirror strategy

--- run_cnn_model_gedi.py
+++ run_cnn_model_gedi.py
@@ -94,39 +94,40 @@ def wandb_config(model_name, backbone, batch_size, learning_rate, nchannels):
       "backbone": backbone
     }
 def create_model(model_name, backbone, learning_rate, nchannels):
-    if model_name == 'fpn':
-        input = tf.keras.Input(shape=(None, None, 3))
-        conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
-        basemodel = FPN(backbone, encoder_weights='imagenet', activation='relu', classes=1)
-        output = basemodel(conv1)
-        model = tf.keras.Model(input, output, name=model_name)
+    mirrored_strategy = tf.distribute.MirroredStrategy()
+    with mirrored_strategy.scope():
+        if model_name == 'fpn':
+            input = tf.keras.Input(shape=(None, None, 3))
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+            basemodel = FPN(backbone, encoder_weights='imagenet', activation='relu', classes=1)
+            output = basemodel(conv1)
+            model = tf.keras.Model(input, output, name=model_name)
 
-    elif model_name == 'unet':
-        input = tf.keras.Input(shape=(64, 64, nchannels))
-        conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
-        if backbone == 'None':
-            basemodel = Unet(input_shape=(64, 64, 3), encoder_weights='imagenet', activation='relu')
-        else:
-            basemodel = Unet(backbone, input_shape=(64, 64, 3), encoder_weights='imagenet', activation='relu')
-        output = basemodel(conv1)
-        model = tf.keras.Model(input, output, name=model_name)
+        elif model_name == 'unet':
+            input = tf.keras.Input(shape=(64, 64, nchannels))
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+            if backbone == 'None':
+                basemodel = Unet(input_shape=(64, 64, 3), encoder_weights='imagenet', activation='relu')
+            else:
+                basemodel = Unet(backbone, input_shape=(64, 64, 3), encoder_weights='imagenet', activation='relu')
+            output = basemodel(conv1)
+            model = tf.keras.Model(input, output, name=model_name)
 
-    elif model_name == 'linknet':
-        input = tf.keras.Input(shape=(None, None, 3))
-        conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
-        basemodel = Linknet(backbone, encoder_weights='imagenet', activation='relu', classes=1)
-        output = basemodel(conv1)
-        model = tf.keras.Model(input, output, name=model_name)
+        elif model_name == 'linknet':
+            input = tf.keras.Input(shape=(None, None, 3))
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input)
+            basemodel = Linknet(backbone, encoder_weights='imagenet', activation='relu', classes=1)
+            output = basemodel(conv1)
+            model = tf.keras.Model(input, output, name=model_name)
 
-    elif model_name == 'pspnet':
-        input = tf.keras.Input(shape=(None, None, 3))
-        input_resize = tf.keras.layers.Resizing(384,384)(input)
-        conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input_resize)
-        basemodel = PSPNet(backbone, activation='relu', classes=1)
-        output = basemodel(conv1)
-        output_resize = tf.keras.layers.Resizing(256,256)(output)
-        model = tf.keras.Model(input, output_resize, name=model_name)
-    model.summary()
+        elif model_name == 'pspnet':
+            input = tf.keras.Input(shape=(None, None, 3))
+            input_resize = tf.keras.layers.Resizing(384,384)(input)
+            conv1 = tf.keras.layers.Conv2D(3, 3, activation = 'linear', padding = 'same', kernel_initializer = 'he_normal')(input_resize)
+            basemodel = PSPNet(backbone, activation='relu', classes=1)
+            output = basemodel(conv1)
+            output_resize = tf.keras.layers.Resizing(256,256)(output)
+            model = tf.keras.Model(input, output_resize, name=model_name)
     optimizer = tf.optimizers.SGD(learning_rate=learning_rate)
     model.compile(optimizer, loss=masked_mse, metrics= masked_mse)
     return model
@@ -146,9 +147,8 @@ if __name__=='__main__':
     learning_rate = args.lr
     nchannels = args.nc
     set_global_seed()
-    mirrored_strategy = tf.distribute.MirroredStrategy()
-    with mirrored_strategy.scope():
-        model = create_model(model_name, backbone, learning_rate, nchannels)
+
+    model = create_model(model_name, backbone, learning_rate, nchannels)
     MAX_EPOCHS = 100
     options = tf.data.Options()
     options.experimental_distribute.auto_shard_policy = tf.data.experimental.AutoShardPolicy.DATA
