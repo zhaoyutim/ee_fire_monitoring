3607e816cbe305122e17a71f001091c5dc94c885
赵宇 <zhaoyu@n171-p200.eduroam.kth.se>
2022-11-09 15:23:32 +0100

    adsad

--- main_gedi_client.py
+++ main_gedi_client.py
@@ -1,16 +1,32 @@
 import argparse
+import time
+from multiprocessing import Pool
 
+import geopandas as gpd
 from GEDIClient.GEDIClient import GEDIClient
+
+def gedi_download(region, id):
+    print(id)
+    # time.sleep(3)
+    json_path = 'gedi_sample_json/'+region+'_json.geojson'
+    gedi_client=GEDIClient()
+    # gedi_client.visualize_geojson(json_path)
+    file_url_list = gedi_client.query_with_json(json_path, id)
+    gedi_client.download(json_path, file_url_list, id)
 
 if __name__=='__main__':
     parser = argparse.ArgumentParser(description='Process some integers.')
-    parser.add_argument('-id', type=int, help='Model to be executed')
+    parser.add_argument('-region', type=str, help='region to be executed')
     args = parser.parse_args()
-    id = args.id
-    gedi_client=GEDIClient()
-    gedi_client.visualize_geojson('gedi_sample_json/custom_region_json.geojson')
-    file_url_list = gedi_client.query_with_json('gedi_sample_json/custom_region_json.geojson', id)
-    gedi_client.download('gedi_sample_json/custom_region_json.geojson', file_url_list, id)
+    region = args.region
+    json_path = 'gedi_sample_json/'+region+'_json.geojson'
+    n_geometries = len(gpd.read_file(json_path))
+    pool = Pool(processes=n_geometries)
+    for i in range(n_geometries):
+        pool.apply_async(gedi_download, args=(region, i))
+    print('test')
+    pool.close()
+    pool.join()
     # gedi_client.concatcsv('subsets/*.csv')
     # gedi_client.csv_to_tiff('subsets/aca_gedi_l4a0.csv')
 
